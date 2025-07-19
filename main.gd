f6ece9ef6b20daf5956df014c4bad1de87a3bca7
RageOfShizuno <eleonorabiuso@hotmail.it>
2025-07-19 15:20:27 +0200

    commented out the print

--- main.gd
+++ main.gd
@@ -1,10 +1,7 @@
-# Scene size:
-# X: 1152
-# Y: 648
 
 extends Node2D
 
 func _ready():
 	var viewport_size = get_viewport_rect().size
-	print("Larghezza viewport: ", viewport_size.x)
-	print("Altezza viewport: ", viewport_size.y)
+#	print("Larghezza viewport: ", viewport_size.x)
+#	print("Altezza viewport: ", viewport_size.y)
