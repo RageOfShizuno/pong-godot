cfa3de75ea3e6e3129cfc551ca9557c87b1be07e
RageOfShizuno <eleonorabiuso@hotmail.it>
2025-07-19 15:25:39 +0200

    left and right paddle move independently with different inputs

--- paddle.gd
+++ paddle.gd
@@ -1,12 +1,19 @@
-# This works to move the paddle up and down, and it limits the movement in the screen, but the "beginning" of the paddle is also considered the end of it, so it still phases through the bottom of the screen.
-# Needs fixing
-
-
 extends CharacterBody2D
 
 @export var speed := 300.0
+@export_enum("left", "right") var paddle_side := "left"  # ← Scegli dal menu laterale
 
 func _physics_process(delta):
-	var input_direction := Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var input_up := ""
+	var input_down := ""
+
+	if paddle_side == "left":
+		input_up = "paddle_left_up"
+		input_down = "paddle_left_down"
+	else:
+		input_up = "paddle_right_up"
+		input_down = "paddle_right_down"
+
+	var input_direction := Input.get_action_strength(input_down) - Input.get_action_strength(input_up)
 	velocity = Vector2(0, input_direction * speed)
 	move_and_slide()
